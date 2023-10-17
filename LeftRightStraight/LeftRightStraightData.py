023449e7a228e15a070630414144a3ccf14ae42c
apurvakhangal <apurvakhangal29@gmail.com>
2023-10-18 01:53:42 +0530

    uploading

---
+++ LeftRightStraight/LeftRightStraightData.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+"""
+Left View-1
+Right View-0
+Straight-2
+Accuracy- 59
+"""
+data = pd.read_csv('D:\\MajorProject\\mllr\\LeftRightStraightData.csv')
+data1 = data.dropna()
+x = data1[['LowAlpha','LowAlphaPhase','HighAlpha','HighAlphaPhase','LowBeta','LowBetaPhase','HighBeta','HighBetaPhase','LowGamma','LowGammePhase','HighGamma','HighGammaPhase']].values
+y = data1[['View']]  
+
+print(data1)
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = svm.SVC(kernel='rbf')
+model.fit(xtrain, ytrain.values.ravel())  # Use ravel() to reshape ytrain
+
+from sklearn.metrics import accuracy_score
+
+# Make predictions on the test data
+y_pred = model.predict(xtest)
+
+# Calculate the accuracy score
+accuracy = accuracy_score(ytest, y_pred)
+print("Accuracy:",accuracy)
+
+# Make predictions on the test data
+y_pred= model.predict(xtest)
+
+joblib.dump(model,'LeftRightStraightData.pkl')
+print("Model saved successfully")
